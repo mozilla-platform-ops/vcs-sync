d268afce1513e3c1ae0a7c6f64868f1803b7e391
davehouse <dhouse@mozilla.com>
2019-07-02 09:02:06 -0600

    note required env file

--- test.sh
+++ test.sh
@@ -3,4 +3,8 @@
 git clone https://github.com/mozilla/release-services.git
 hg clone https://hg.mozilla.org/build/mozharness
 
-docker-compose up
+docker-compose up || \
+    printf "# vcssync.env must contain variables like:
+RELENGAPI_INSERT_HGGIT_MAPPINGS_TASKCLUSTER_CLIENT_ID=project/releng/services/mapper/bug1555970
+RELENGAPI_INSERT_HGGIT_MAPPINGS_TASKCLUSTER_ACCESS_TOKEN=tokenstring\n"
+
